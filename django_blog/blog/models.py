a59046814569823ba7d5e3144853146bf311c4af
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-05-12 10:40:39 -0400

    Blog app, models, migrations, superuser.  lib: django-taggit, Pillow

---
+++ django_blog/blog/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+from django.core.validators import MinLengthValidator, MaxLengthValidator, RegexValidator
+
+# Create your models here.
+
+# Users Profile Model #
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique = True)
+    bio = models.TextField(max_length=500, blank=True, unique=True)
+    profile_pic = models.ImageField(upload_to="profile_pics", blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} User Profile"
+    
+    @property
+    def username(self):
+        return self.user.username
+    
+# Posts Model #
+STATUS_LIST = [
+    ('draft', 'Draft'),
+    ('published', 'Published'),
+    ('archvied', 'Archived')
+]
+class Post(models.Model):
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, blank=False, null=False,
+        unique=True, validators=[
+        MinLengthValidator(5), MaxLengthValidator(100),
+        RegexValidator(regex = '^[a-zA-Z0-9].*$',
+                        message="Title must start with a letter or numner")
+    ])
+    content = models.TextField(blank=False, null=False,
+        unique=True, validators=[
+        MinLengthValidator(15), MaxLengthValidator(4096)
+    ])
+    status = models.CharField(max_length=10, choices=STATUS_LIST,
+                                default='draft')
+    comments = []
+    tags = TaggableManager()
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author.username}"
+    
+# Comments Model # 
+class Comment(models.Model):
+    author = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    reply_to = models.ForeignKey('self', null=True, blank=True,
+                    default=None, on_delete=models.PROTECT)
+    content = models.TextField(blank=False, null=False,
+                validators=[MinLengthValidator(2), 
+                            MaxLengthValidator(2048)
+    ])
+    status = models.CharField(max_length=10, choices=STATUS_LIST,
+                            default='draft')
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"by - {self.author.username} post - {self.post.pk}"
+
+# Likes Models #
+LIKE_STATUS = [
+    ('like', 'Like'),
+    ('dislike', 'Dislike')
+]
+# Likes Post Model #
+class PostLike(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    status = models.CharField(max_length=7, 
+                choices=LIKE_STATUS, default='like')
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Prevent Multiple Likes
+    class Meta:
+        unique_together = ['user', 'post']
+
+    def __str__(self):
+        return f"{self.user.username} - {self.status} - {self.post.pk}"
+
+# Likes Comment Model # 
+class CommentLike(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    status = models.CharField(max_length=7,
+                choices=LIKE_STATUS, default='like')
+    created_at = models.DateTimeField(auto_now_add=True)
+    # Prevent Multiple Likes
+    class Meta:
+        unique_together = [('user', 'comment')]
+    #   db_table = "CommentLike"
+
+    def __str__(self):
+        return f"{self.user.username} - {self.status} - {self.post.pk}"
