4923e93d87c543008ef83163e393d4d72c1f9c2a
l2DWOLF <123218119+l2DWOLF@users.noreply.github.com>
2025-07-28 20:11:46 -0400

    fix - static

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -58,6 +58,7 @@ ROOT_URLCONF = 'django_blog.urls'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
@@ -183,7 +184,5 @@ TIME_ZONE = 'America/New_York'
 USE_I18N = True
 
 USE_TZ = True
-
-STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
